ebfa943769b716a19aaa417a1e8b128c5a037f37
Tasfiqul Asif <tasfiq.asif.23@gmail.com>
2024-09-12 05:52:41 +0600

    cors fixed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8000 ;
 const corsOptions = {
   origin: ['http://localhost:5173', 'http://localhost:5174'],
   credentials: true,
-  optionSuccessStatus: 200,
+  optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
