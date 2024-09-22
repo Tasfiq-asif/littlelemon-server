26dd70521f1bc116bec19f5b8d5232dfed1222ab
Tasfiqul Asif <tasfiq.asif.23@gmail.com>
2024-09-23 00:33:59 +0600

    final

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8000 ;
 
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174','https://littlelemon-6efed.web.app','https://littlelemon-6efed.firebaseapp.com'],
   credentials: true,
   optionsSuccessStatus: 200,
 };
